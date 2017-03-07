6cf74082c18dcae2755bfd6b80dd179ac4f8dda0
rizki113070213 <rizki113070213@gmail.com>
2017-03-07 13:47:27 +0700

    Delete routes.rb

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :blogs
-=======
->>>>>>> de6183d9b247f05eaff0722721bfbd5eafd64230
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
