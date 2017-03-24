b997b857520a0c371b8beb2fe1b5fc4856840842
rizki <rizki@adhigunas-Mac-mini.local>
2017-03-24 10:23:49 +0700

    db recovery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,4 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
-
-  include do
-    before_filter :configure_permitted_parameters, if: :devise_controller?
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :roles])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :roles])
-  end
 end
