ba789810f92dd32e56420d51cd5d61d2ce39502a
rizki <rizki@adhigunas-Mac-mini.local>
2017-03-24 09:58:58 +0700

    move to model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-  include DeviseWhitelist
 
-  
+
+  # #include do
+  #   before_filter :configure_permitted_parameters, if: :devise_controller?
+  # #end
+  #
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :roles])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :roles])
+  # end
 end
