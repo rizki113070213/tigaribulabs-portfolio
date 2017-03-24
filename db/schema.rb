2530cea0fd364ba00c3c9962cfc63fd89a40aa65
rizki <rizki@adhigunas-Mac-mini.local>
2017-03-24 13:21:02 +0700

    fixed db users

--- db/schema.rb
+++ db/schema.rb
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170324101958) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -82,7 +83,6 @@ ActiveRecord::Schema.define(version: 20170324101958) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "name"
     t.string   "roles"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
